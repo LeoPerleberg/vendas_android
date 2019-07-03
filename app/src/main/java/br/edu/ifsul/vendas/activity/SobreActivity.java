2f4b4a735672abc4a24549d63b0bba5e6a4c6b5b
Leonardo Perleberg <leonardoperleberg@outlook.com>
2019-07-03 08:46:12 -0300

    aplicativo quase finalizado

--- app/src/main/java/br/edu/ifsul/vendas/activity/SobreActivity.java
+++ app/src/main/java/br/edu/ifsul/vendas/activity/SobreActivity.java
@@ -2,6 +2,7 @@ package br.edu.ifsul.vendas.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import br.edu.ifsul.vendas.R;
 
@@ -15,5 +16,17 @@ public class SobreActivity extends AppCompatActivity {
         //botão de voltar na barra superior
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                break;
+
+        }
+        return true;
     }
 }
